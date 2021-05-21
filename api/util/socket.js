6f41c65bdf50de957da9edb50cc03091020f823a
christiangns <christian.santos@focustextil.com.br>
2021-05-21 07:51:25 -0300

    subindo server

--- api/util/socket.js
+++ api/util/socket.js
@@ -1,5 +1,5 @@
-const http = require('http');
-const { Server } = require('socket.io');
+import http from 'http';
+import { Server } from 'socket.io';
 
 export default class SocketServer {
     #io
@@ -11,18 +11,29 @@ export default class SocketServer {
     async start () {
         const server = http.createServer( (req, res) => {
             res.writeHead(200, {
-                'Acess-Control-Allow-Origin'
+                'Acess-Control-Allow-Origin': '#',
+                'Acess-Control-Allow-Methods': 'OPTIONS, POST, GET'
             })
             res.end('hey there!')
         })
 
-        this.#io - new Server(server, {
+        this.#io = new Server(server, {
 
             cors: {
                 origin: '#',
                 credentials: false
             }
 
+        })
+
+        const room = this.#io.of('/room') 
+        room.on('connection', socket => {
+            socket.emit('userConnection', 'socket id is connected' + socket.id)
+
+            socket.on('joinRoom', (dados) => {
+                console.log('estes são os dados recebidos', dados)
+
+            })
         })
 
         return new Promise( (res, rej) => {
